8e395e06796ee6c58ef6750f2e352af01cd71aa0
Lucas Pleß <lucas@xleave.de>
2016-01-17 20:20:54 +0100

    cleanup

--- js/ptz.js
+++ js/ptz.js
@@ -3,8 +3,6 @@
  */
 
 var btnCount = 48;
-var url = "http://192.168.191.21/cgi-bin/aw_ptz?cmd=/cgi-bin/aw_ptz?cmd=%23R02&res=1";
-
 
 var pad = function(number) {
     if(number < 10) {
@@ -24,13 +22,10 @@ var btnRow = function(container, iframe, ip) {
 };
 
 $(function () {
-
     btnRow("#kamera1", "#hidden_iframe1", "192.168.191.21");
     btnRow("#kamera2", "#hidden_iframe2", "192.168.191.22");
     btnRow("#kamera3", "#hidden_iframe3", "192.168.191.23");
     btnRow("#kamera4", "#hidden_iframe4", "192.168.191.24");
-
-
 });
 
 
